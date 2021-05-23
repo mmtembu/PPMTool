dacce5b361b6fc0289350b5e99d021710c913547
mmtembu <mmtembu@student.wethinkcode.co.za>
2021-05-23 02:27:54 +0200

    Delete an existing project.

--- PPMToolFullStack/src/main/java/com/mmtembu/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/mmtembu/ppmtool/services/ProjectService.java
@@ -33,4 +33,12 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Cannot find Project with ID '"+projectId+"'. This project does not exist.");
+        }
+        projectRepository.delete(project);
+    }
 }
--- PPMToolFullStack/src/main/java/com/mmtembu/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/mmtembu/ppmtool/web/ProjectController.java
@@ -49,4 +49,15 @@ public class ProjectController {
     public Iterable<Project> getAllProjects(){
         return projectService.findAllProjects();
     }
+
+//    @DeleteMapping("/{projectId}")
+    @RequestMapping(
+            value = "/{projectId}",
+            produces = "application/json",
+            method = {RequestMethod.DELETE}
+    )
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted", HttpStatus.OK);
+    }
 }
