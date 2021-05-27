7f5c41ecf219372de5331daa3874054c65b3b19f
mmtembu <mmtembu@student.wethinkcode.co.za>
2021-05-27 13:50:52 +0200

    Bringing bootstrap.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
