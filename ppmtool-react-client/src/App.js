d2bba5f612e189764241cac9254181affb4c1184
mmtembu <mmtembu@student.wethinkcode.co.za>
2021-05-28 13:08:37 +0200

    Branch16 not fully completed.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <Header/>
-    <Dashboard/> 
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Dashboard/>
+      </div>
+    </Router>
   );
 }
 
