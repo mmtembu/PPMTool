9454d97bda943de5df99f20b997ecfc1053d0080
mmtembu <mmtembu@student.wethinkcode.co.za>
2021-05-31 04:10:53 +0200

    React, Router, first functional component.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,15 @@ import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from './component/Project/AddProject';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header/>
-        <Dashboard/>
+        <Route exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/addProject" component={AddProject}/>
       </div>
     </Router>
   );
