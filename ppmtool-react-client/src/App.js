bb3c08d7e4a35dae5f50b2d5500b048c262de421
mmtembu <mmtembu@student.wethinkcode.co.za>
2021-06-01 14:08:16 +0200

    AddProject Component, control form.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Dashboard from './component/Dashboard';
-import Header from './component/Layout/Header';
+import "./App.css";
+import Dashboard from "./component/Dashboard";
+import Header from "./component/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import AddProject from './component/Project/AddProject';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./component/Project/AddProject";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header/>
-        <Route exact path="/dashboard" component={Dashboard}/>
-        <Route exact path="/addProject" component={AddProject}/>
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
       </div>
     </Router>
   );
